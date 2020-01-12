fcc414ef436dc0b0a07a7e81bea315ec873a26be
Maximilian Chanba <chanba@iMac-Maximilian.local>
2020-01-12 20:38:28 +0300

    Add router

--- src/App.js
+++ src/App.js
@@ -1,61 +1,15 @@
 import React, { Component } from "react";
-import injectSheet from "react-jss";
-import styles from "./styles";
-import SocialBox from "./sections/SocialBox";
-import EmailBox from "./sections/EmailBox";
-import Header from "./sections/Header";
-import PhotoBox from "./sections/PhotoBox";
-import InfoBox from "./sections/InfoBox";
-import SupGuys from "./sections/SupGuys";
-import StarBig from "./sections/StarBig";
-import MaxChanba from "./sections/MaxChanba";
-import StarSmall from "./sections/StarSmall";
-import Pluses from "./sections/Pluses";
+import { Route, Switch, BrowserRouter } from "react-router-dom";
+import Main from "./Main";
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = { x: 0, y: 0, mouseOn: false };
-  }
-
-  onMouseMove = e => {
-    this.setState({ x: e.clientX - 20, y: e.clientY - 20 });
-  };
-
+export default class App extends Component {
   render() {
-    const { classes } = this.props;
-    const { mouseOn, x, y } = this.state;
-    const isMobile = window.innerWidth < 480;
-    const style = isMobile
-      ? { cursor: "default", maxWidth: 375, overflowX: "hidden !important" }
-      : {};
-
     return (
-      <div
-        className={classes.app}
-        style={style}
-        onMouseMove={e => this.onMouseMove(e)}
-        onMouseEnter={() => this.setState({ mouseOn: true })}
-        onMouseLeave={() => this.setState({ mouseOn: false })}
-      >
-        {!isMobile && mouseOn && (
-          <div className={classes.circle} style={{ top: y, left: x }} />
-        )}
-        <Header />
-        <MaxChanba />
-        <PhotoBox />
-        <StarBig />
-        <StarSmall />
-        <SocialBox />
-        <EmailBox />
-        <SupGuys />
-        <InfoBox />
-        {isMobile && <Pluses isThree />}
-        {isMobile && <Pluses isThree={false} />}
-      </div>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={Main} />
+        </Switch>{" "}
+      </BrowserRouter>
     );
   }
 }
-
-export default injectSheet(styles)(App);
---
+++ src/Main.jsx
@@ -0,0 +1,61 @@
+import React, { Component } from "react";
+import injectSheet from "react-jss";
+import styles from "./styles";
+import SocialBox from "./sections/SocialBox";
+import EmailBox from "./sections/EmailBox";
+import Header from "./sections/Header";
+import PhotoBox from "./sections/PhotoBox";
+import InfoBox from "./sections/InfoBox";
+import SupGuys from "./sections/SupGuys";
+import StarBig from "./sections/StarBig";
+import MaxChanba from "./sections/MaxChanba";
+import StarSmall from "./sections/StarSmall";
+import Pluses from "./sections/Pluses";
+
+class Main extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { x: 0, y: 0, mouseOn: false };
+  }
+
+  onMouseMove = e => {
+    this.setState({ x: e.clientX - 20, y: e.clientY - 20 });
+  };
+
+  render() {
+    const { classes } = this.props;
+    const { mouseOn, x, y } = this.state;
+    const isMobile = window.innerWidth < 480;
+    const style = isMobile
+      ? { cursor: "default", maxWidth: 375, overflowX: "hidden !important" }
+      : {};
+
+    return (
+      <div
+        className={classes.app}
+        style={style}
+        onMouseMove={e => this.onMouseMove(e)}
+        onMouseEnter={() => this.setState({ mouseOn: true })}
+        onMouseLeave={() => this.setState({ mouseOn: false })}
+      >
+        {!isMobile && mouseOn && (
+          <div className={classes.circle} style={{ top: y, left: x }} />
+        )}
+        <Header />
+        <MaxChanba />
+        <PhotoBox />
+        <StarBig />
+        <StarSmall />
+        <SocialBox />
+        <EmailBox />
+        <SupGuys />
+        <InfoBox />
+        {isMobile && <Pluses isThree />}
+        {isMobile && <Pluses isThree={false} />}
+      </div>
+    );
+  }
+}
+
+export default injectSheet(styles)(Main);
