57b11a41bf605b77eb06e60674772c984bb37247
Maximilian Chanba <chanba@iMac-Maximilian.local>
2020-01-13 00:49:53 +0300

    Add fonts

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { Route, Switch, BrowserRouter } from "react-router-dom";
 import Main from "./Main";
+import "./fonts.css";
 
 export default class App extends Component {
   render() {
